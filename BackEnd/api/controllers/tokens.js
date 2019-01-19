64a824ab8121114fe0a71ea6c8b2e3409b185469
carlos830 <carlos.rodriguezt@utem.cl>
2019-01-19 20:12:27 -0300

    Crecion de 3 funciones

--- BackEnd/api/controllers/tokens.js
+++ BackEnd/api/controllers/tokens.js
@@ -18,7 +18,7 @@ function authenticate(req, res) {
                     if (token.password == password) {
                         res.status(200).send({ token });
                     } else {
-                        res.status(404).send({ message: "El usuario no ha podido loguearse" })
+                        res.status(404).send({ message: "Usuario no pudo logearse" });
                     }
 
                 }
@@ -26,11 +26,74 @@ function authenticate(req, res) {
 
         )
         .catch(err => {
-            console.log(err);
+            res.status(500).send({ err });
+        })
+}
+
+
+function forgot(req, res) {
+    const params = req.body;
+    const rut = params.rut;
+    const email = params.email;
+
+    Tokens.findOne({
+        where: { rut: rut }
+    })
+
+    .then(token => {
+        if (!token) {
+            res.status(400).send({ message: "Usuario no existe" });
+        } else {
+            if (token.email == email) {
+                res.status(200).send({ token })
+            } else {
+                res.status(404).send("Correo no coincide");
+            }
+        }
+    })
+
+    .catch(err => {
+        res.status(500).send("Error interno");
+    })
+
+}
+
+function chance(req, res) {
+    const params = req.body;
+    const rut = params.rut;
+    const password = params.password;
+    const newPassword = params.temporal
+
+    Tokens.findOne({
+        where: { rut: rut }
+    })
+
+    .then(token => {
+                if (!token) { //no existe usuario
+                    res.status(400).send({ message: "El usuario no existe" });
+                } else { //Si existe comprobar contraseña
+                    if (token.password == password) {
+                        token.updateAttributes({
+                            password: newPassword
+                        })
+                        res.status(200).send({ token });
+                    } else {
+                        res.status(404).send({ message: "El usuario no ha podido loguearse" });
+                    }
+
+                }
+            }
+
+        )
+        .catch(err => {
+            res.status(500).send({ err });
         })
 }
 
 
 module.exports = {
-    authenticate
+    authenticate,
+    forgot,
+    chance
+
 }
--- BackEnd/api/routes/tokens.js
+++ BackEnd/api/routes/tokens.js
@@ -3,4 +3,6 @@ const TokenController = require('../controllers/tokens');
 
 module.exports = (app) => {
     app.post('/api/v1/authentication/authenticate', TokenController.authenticate);
+    app.post('/api/v1/authentication/forgot', TokenController.forgot);
+    app.post('/api/v1/authentication/passsword/change', TokenController.chance);
 }
