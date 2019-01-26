8d91d6f61698997bf88a1afb83d9a91e3a4c78d2
carlos830 <carlos.rodriguezt@utem.cl>
2019-01-26 06:06:41 -0300

    correccion

--- BackEnd/api/controllers/tokens.js
+++ BackEnd/api/controllers/tokens.js
@@ -25,7 +25,7 @@ function authenticate(req, res) {
     const password = params.password;
     const hashOld = encriptar(password);
     sequelize.query(`select rut, role, "apiKey" from tokens
-   where password = '${hashOld}'  `, { type: Sequelize.QueryTypes.SELECT })
+   where password = '${hashOld}' and rut = ${rut}  `, { type: Sequelize.QueryTypes.SELECT })
         .then(token => {
             if (token == '') {
                 res.status(400).send({ message: "El usuario no existe" });
