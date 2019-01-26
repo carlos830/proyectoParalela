e8fb7a5040e962486875d9381ab8b9c628fed90d
carlos830 <carlos.rodriguezt@utem.cl>
2019-01-26 04:21:33 -0300

    respaldo

---
+++ BackEnd/api/controllers/index.js
@@ -0,0 +1,15 @@
+const students = require('./students');
+const courses = require('./courses');
+const finished_courses = require('./finished_courses');
+const tokens = require('./tokens');
+const teachers = require('./teachers');
+const subjects = require('./subjects');
+
+module.exports = {
+    students,
+    courses,
+    finished_courses,
+    tokens,
+    teachers,
+    subjects
+}
