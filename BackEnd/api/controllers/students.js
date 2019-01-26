34cec33b1a8d55f3f6afa9a372579add421b6258
carlos830 <carlos.rodriguezt@utem.cl>
2019-01-25 23:59:58 -0300

    respaldo

--- BackEnd/api/controllers/students.js
+++ BackEnd/api/controllers/students.js
@@ -10,9 +10,8 @@ function Student(req, res) { //revisar
     const rut = params.rut;
     const apiKey = params.apiKey;
 
-    sequelize.query(`select a1.birthdate, a1.first_name , a1.gender, a1.last_name, a1.rut from students a1
-    inner join tokens as a2 as on a1.rut = a2.rut
-    where a1.rut =${rut} `, { type: Sequelize.QueryTypes.SELECT })
+    sequelize.query(`select a1.birthdate, a1.first_name as firstName, a1.gender, a1.last_name as lastName, 
+    a1.rut from students a1 inner join tokens a2 on a1.rut = a2.rut where a2.rut = ${rut} `, { type: Sequelize.QueryTypes.SELECT })
 
     .then(student => {
 
